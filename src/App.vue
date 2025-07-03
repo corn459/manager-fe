3680c6dd1bcd24b7b7734750f322b700a0b1cc70
Tayllen13514 <x13139395413@qq.com>
2025-07-03 21:14:22 +0800

    feat:storage二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
   name: "APP",
+  mounted() {
+    this.$storage.setItem("userInfo", { age: 18, name: "tom" });
+    console.log("user=>", this.$storage.getItem("userInfo"));
+    this.$storage.clearAll();
+  },
 };
 </script>
 
